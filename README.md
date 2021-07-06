3906829d92f08bee0aa345329b89750bf3d0d8eb
yuus95 <kkad45@naver.com>
2021-07-06 21:42:33 +0900

    0706 환경설정 완료 2틀걸림

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QueryDsl
+
+# QEntity 안잡힐 떄 .idea  .gradle 지우고 다시 실행시키기
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,53 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
+
+
+
+
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		
+		
+		//QHello qHello = new QHello("h"); 밑에 문장과 같은 표현
+		QHello qHello = QHello.hello.hello;
+		
+		
+		
+		Hello result = query
+				.selectFrom(qHello) // 큐타입 넣기
+				.fetchOne();
+
+		Assertions.assertEquals(hello,result);
+		Assertions.assertEquals(hello.getId(),result.getId());
+
+
+
+
 	}
 
 }
